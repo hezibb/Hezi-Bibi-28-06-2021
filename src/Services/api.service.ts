f82e0505edbfaa88084ced620c9f66f47f5beb4e
hezibb <hezibb@gmail.com>
2021-06-28 00:35:28 +0300

    'changeprotocol'

--- src/Services/api.service.ts
+++ src/Services/api.service.ts
@@ -10,6 +10,7 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
+   protocol:string = window.location.protocol;
   
   getRequest(url, q?) {
     const params = new HttpParams({fromObject: {apikey: API_KEY, q, metric: 'true'}});
@@ -18,23 +19,23 @@ export class ApiService {
   }
 
   getGeoPosition(lat: number, lng: number): Observable<any> {
-    const url = `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search`;
+    const url = `${this.protocol}//dataservice.accuweather.com/locations/v1/cities/geoposition/search`;
     return this.getRequest(url, `${lat},${lng}`);
   }
 
   getAutoComplete(key: string): Observable<any> {
-    const url = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete`;
+    const url = `${this.protocol}//dataservice.accuweather.com/locations/v1/cities/autocomplete`;
     return this.getRequest(url, `${key}`);
   }
 
   get5DaysOfForecasts(key: string): Observable<any> {
-    const url = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}`;
+    const url = `${this.protocol}//dataservice.accuweather.com/forecasts/v1/daily/5day/${key}`;
     return this.getRequest(url);
   }
 
 
   getCurrentConditions(key: string): Observable<any> {
-    const url = `http://dataservice.accuweather.com/currentconditions/v1/${key}`;
+    const url = `${this.protocol}//dataservice.accuweather.com/currentconditions/v1/${key}`;
     return this.getRequest(url);
   }
 
